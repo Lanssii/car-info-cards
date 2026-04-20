cc1fdfd4b046c63795c3f31f628335887577f509
lana shotashvili <shotashvililana@gmail.com>
2026-04-20 19:57:19 +0400

    feat: update HTML structure, add styling, and integrate car images in Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Component from "./Component/Component.jsx";
-
+import sedanIcon from "./assets/car1-card.png";
+// import sedanIcon from './assets/car2-card.png';
+// import sedanIcon from './assets/car3-card.png';
 function App() {
   return (
     <>
-      <Component />
+      <Component imageSrc={sedanIcon} />
     </>
   );
 }
--- src/Component/Component.jsx
+++ src/Component/Component.jsx
@@ -1,9 +1,17 @@
 import "./Component.css";
 
-function Component() {
+function Component(props) {
   return (
     <>
-      <h1>Lana</h1>
+      <div className="card-container">
+        <img className="car-image" src={props.imageSrc} alt="Sedan icon" />
+        <h1 className="title">Sedans</h1>
+        <p className="info">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <button className="more-btn">Learn More</button>
+      </div>
     </>
   );
 }
