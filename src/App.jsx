b71b3f703306d8884b9faf631c0164c6b1f54503
lana shotashvili <shotashvililana@gmail.com>
2026-04-20 20:04:34 +0400

    feat: create 3 car-cards using Component and proms

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import Component from "./Component/Component.jsx";
 import sedanIcon from "./assets/car1-card.png";
-// import sedanIcon from './assets/car2-card.png';
-// import sedanIcon from './assets/car3-card.png';
+import suvsIcon from "./assets/car2-card.png";
+import luxuryIcon from "./assets/car3-card.png";
 function App() {
   return (
     <>
-      <Component imageSrc={sedanIcon} />
+      <div className="cards-container">
+        <Component
+          imageSrc={sedanIcon}
+          title="SEDANS"
+          info=" Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip."
+        />
+        <Component
+          imageSrc={suvsIcon}
+          title="SUVS"
+          info="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+        />
+        <Component
+          imageSrc={luxuryIcon}
+          title="LUXURY"
+          info="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. "
+        />
+      </div>
     </>
   );
 }
--- src/Component/Component.jsx
+++ src/Component/Component.jsx
@@ -5,11 +5,8 @@ function Component(props) {
     <>
       <div className="card-container">
         <img className="car-image" src={props.imageSrc} alt="Sedan icon" />
-        <h1 className="title">Sedans</h1>
-        <p className="info">
-          Choose a sedan for its affordability and excellent fuel economy. Ideal
-          for cruising in the city or on your next road trip.
-        </p>
+        <h1 className="title">{props.title}</h1>
+        <p className="info">{props.info}</p>
         <button className="more-btn">Learn More</button>
       </div>
     </>
